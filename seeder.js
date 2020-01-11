6b6d080db064e426d44cf4a092d5fa1f886bdb27
DeathDoctor <deathdoctor4499@gmail.com>
2020-01-11 19:05:34 +0100

    Data model for Courses created

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: "./config/config.env" });
 
 // Load models
 Bootcamp = require("./models/Bootcamp");
+Course = require("./models/Course");
 
 // DB connection
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,10 +22,14 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
 
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Data imported".green.inverse);
     process.exit();
   } catch (error) {
@@ -35,6 +40,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Data Deleted".green.inverse);
     process.exit();
   } catch (error) {
