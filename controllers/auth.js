9f025a0cfce0b5a48012bb7b0ea625ef0d2e1bed
amine-blaha <deathdoctor4499@gmail.com>
2020-01-19 17:16:32 +0100

    hashing password

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,9 @@ const User = require("../models/User");
 // @route       /api/v1/auth/register
 // @access      Public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ succes: true });
+  const { name, email, password, role } = req.body;
+
+  const user = await User.create({ name, email, password, role });
+
+  res.status(200).json({ succes: true, user });
 });
